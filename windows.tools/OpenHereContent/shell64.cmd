3b16a6b6dce1b6a79831ccd7547c63528afa5716
Ilya Seleznev <itsuart@gmail.com>
2019-01-25 14:56:56 +0700

    add content handlers for the extensions

---
+++ windows.tools/OpenHereContent/shell64.cmd
@@ -0,0 +1,5 @@
+@echo off
+call c:\tools\mingw64.cmd
+call c:\tools\msys.cmd
+SET PATH="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.13.26128\bin\Hostx64\x64";C:\tools\emacs\bin;c:\tools;%PATH%
+cmd
